73bb20a2cca8b20d462bb5ec84c77d1f7c505650
Radium Zheng <ztl8702@126.com>
2016-01-30 22:43:09 +0800

    beta

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
     angular.module('app', [])
-    .constant("SERVER_API_URL","http://127.0.0.1:5000/api")
+    .constant("SERVER_API_URL","http://fiapi.radiumz.org/api")
     .constant("SERVER_AUDIO_URL","http://mindonglab.github.io/foochow-idioms-audio/audio/");
 }());
 
