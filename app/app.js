d771e354f3f0df840647779776870d8ed14185f2
Radium Zheng <ztl8702@126.com>
2016-02-05 03:58:13 +0800

    new

--- app/app.js
+++ app/app.js
@@ -1,7 +1,32 @@
 (function () {
     "use strict";
-    angular.module('app', [])
+    angular.module('app', ['ngRoute'])
     .constant("SERVER_API_URL","http://fiapi.radiumz.org:2052/api")
-    .constant("SERVER_AUDIO_URL","http://mindonglab.github.io/foochow-idioms-audio/audio/");
+    .constant("SERVER_AUDIO_URL","http://mindonglab.github.io/foochow-idioms-audio/audio/")
+    
+    .config(['$routeProvider',function ($routeProvider) {  
+        $routeProvider  
+            .when('/', {  
+                templateUrl: 'app/views/welcome.tpl.html',  
+                controller: 'homeCtrl'  
+            })  
+            .when('/idiom/:idiomtext*', {  
+                templateUrl: 'app/views/showDetails.tpl.html',  
+                controller: 'detailsCtrl',
+                caseInsensitiveMatch: true
+            })
+            .when('/help', {  
+                templateUrl: 'app/views/help.tpl.html',  
+               // controller: 'mainCtrl'  
+            })
+            .when('/about', {  
+                templateUrl: 'app/views/about.tpl.html',  
+               // controller: 'mainCtrl'  
+            })      
+            .otherwise({  
+                redirectTo: '/'  
+            });  
+    }]); 
+    
 }());
 
--- app/controllers/headerCtrl.js
+++ app/controllers/headerCtrl.js
@@ -1,17 +1,53 @@
 (function ($) {
     "use strict";
-    angular.module('app').controller('headerCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    angular.module('app').controller('headerCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
+        $scope.isOpen = false;
+        
         $scope.homeClicked = function () {
-            $rootScope.$emit('switchToHome');
-
+            $location.path('/');
+            $rootScope.$emit('toggleSidebar', {'state':false});
+        };
+        
+        $scope.aboutClicked = function () {
+            $location.path('/about');
+            $rootScope.$emit('toggleSidebar', {'state':false});
+        };
+        
+        $scope.helpClicked = function () {
+            $location.path('/help');
+            $rootScope.$emit('toggleSidebar', {'state':false});
         };
         
         $scope.init = function () {
-            $(".ms-CommandBar").CommandBar();
+            $(".headerCommands.ms-CommandBar").CommandBar();
         };     
         
         $scope.hamburgerClicked = function () {
             $rootScope.$emit('toggleSidebar');
-        };
+        }; 
+        
+        
+        function changeState(state) {
+            if (state!=undefined) {
+                if (state == false)
+                    $scope.isOpen = false;
+                else if (state == true)
+                    $scope.isOpen = true;
+            } else {
+                $scope.isOpen = !$scope.isOpen;
+            } console.log($scope.isOpen);
+        }
+        
+        var unbind = $rootScope.$on("toggleSidebar", function (e, args) {
+            if (args) {
+                changeState(args.state);
+            } else {
+                changeState();
+            }
+            
+        });
+        $scope.$on('$destroy', unbind);
+        
+      //  $scope.init();
     }]);
 }(jQuery));
